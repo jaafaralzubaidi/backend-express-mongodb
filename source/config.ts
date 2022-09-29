c340c289b9503d5e41cea52e639eb0f6b3dc965e
Jaafar <jaffar.44@hotmail.com>
2022-09-28 18:31:13 -0700

    process.env.PORT

--- source/config.ts
+++ source/config.ts
@@ -34,7 +34,7 @@ const MONGO = {
 
 
 const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost';
-const SERVER_PORT = process.env.SERVER_PORT || 5000;
+const SERVER_PORT = process.env.PORT || 5000;
 
 const SERVER = {
     hostname: SERVER_HOSTNAME,
